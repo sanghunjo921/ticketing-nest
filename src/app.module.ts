26b5966f80dc0254496fa21d76996f6c174b293b
sanghunjo921 <salr921@gmail.com>
2024-03-23 14:22:24 +0900

    fixed circular dependency error

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,37 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TicketModule } from './ticket/ticket.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import dbConfig from './config/postgres.config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 // 관리용 module by domain
 @Module({
-  imports: [AuthModule, UserModule, TicketModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const typeOrmModuleOptions: TypeOrmModuleOptions = {
+          type: configService.get('db.type'),
+          host: configService.get('db.host'),
+          port: configService.get('db.port'),
+          database: configService.get('db.dbName'),
+          username: configService.get('db.username'),
+          password: configService.get('db.password'),
+          synchronize: true,
+          autoLoadEntities: true,
+        } as any;
+        console.log(typeOrmModuleOptions);
+        return typeOrmModuleOptions;
+      },
+    }),
+    AuthModule,
+    UserModule,
+    TicketModule,
+  ],
 })
 export class AppModule {}
