378bd178d67e10411c9afb557c879cd694690802
sanghunjo921 <salr921@gmail.com>
2024-04-15 17:29:18 +0900

    applied redis transaction to purchase logic to handle duplicate key violation error

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -92,93 +92,100 @@ export class UserService {
   }
 
   async reserveTicket(id: string, ticketId: number, quantity: number) {
-    const multi = this.redisService.multi();
+    try {
+      const multi = this.redisService.multi();
 
-    this.logger.log('Retrieval of User data');
-    const user: User = await this.userRepository.findOne({
-      where: { id },
-    });
-    this.logger.log('Found a user');
-
-    if (!user) {
-      multi.discard();
-      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-    }
-
-    const ticketKey: string = `user:${id}-ticket:${ticketId}`;
-    const ticketRemainingKey: string = `ticket:${ticketId}:remaining`;
-    const quantityKey: string = `${ticketKey}:reservedQuantity`;
-
-    this.logger.log('Accessing to Redis to get data');
-    let [ticketData, ticketRemainingData, reservedQuantity] =
-      await this.redisService
-        .mget(ticketKey, ticketRemainingKey, quantityKey)
-        .then((results) =>
-          results.map((item) => (item ? JSON.parse(item) : null)),
-        );
-    this.logger.log('Finished getting data from Redis', {
-      ticketData,
-      ticketRemainingData,
-      reservedQuantity,
-    });
-
-    if (!ticketData) {
-      this.logger.log('Accessing to DB to get data');
-      ticketData = await this.ticketRepository.findOne({
-        where: {
-          id: ticketId,
-        },
+      this.logger.log('Retrieval of User data');
+      const user: User = await this.userRepository.findOne({
+        where: { id },
       });
-      this.logger.log('Finished getting data from DB');
+      this.logger.log('Found a user');
+
+      if (!user) {
+        multi.discard();
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
+
+      const ticketKey: string = `user:${id}-ticket:${ticketId}`;
+      const ticketRemainingKey: string = `ticket:${ticketId}:remaining`;
+      const quantityKey: string = `${ticketKey}:reservedQuantity`;
+
+      this.logger.log('Accessing to Redis to get data');
+      let [ticketData, ticketRemainingData, reservedQuantity] =
+        await this.redisService
+          .mget(ticketKey, ticketRemainingKey, quantityKey)
+          .then((results) =>
+            results.map((item) => (item ? JSON.parse(item) : null)),
+          );
+      this.logger.log('Finished getting data from Redis', {
+        ticketData,
+        ticketRemainingData,
+        reservedQuantity,
+      });
 
       if (!ticketData) {
-        multi.discard();
-        throw new HttpException('Ticket not found', HttpStatus.NOT_FOUND);
+        this.logger.log('Accessing to DB to get data');
+        ticketData = await this.ticketRepository.findOne({
+          where: {
+            id: ticketId,
+          },
+        });
+        this.logger.log('Finished getting data from DB');
+
+        if (!ticketData) {
+          multi.discard();
+          throw new HttpException('Ticket not found', HttpStatus.NOT_FOUND);
+        }
+
+        ticketRemainingData = ticketData.remaining_number;
+        reservedQuantity = quantity;
+
+        if (ticketRemainingData < reservedQuantity) {
+          multi.discard();
+          throw new HttpException('Not enough Tickets', HttpStatus.BAD_REQUEST);
+        }
+
+        ticketRemainingData -= quantity;
+
+        this.logger.log('Accessing to Redis to change data');
+        multi.mset({
+          [ticketKey]: JSON.stringify(ticketData),
+          [ticketRemainingKey]: ticketRemainingData,
+          [quantityKey]: reservedQuantity,
+        });
+        this.logger.log('Finished changing data');
+      } else {
+        if (ticketRemainingData < reservedQuantity) {
+          multi.discard();
+          throw new HttpException('Not enough Tickets', HttpStatus.BAD_REQUEST);
+        }
+        this.logger.log('Accessing to Redis to change data');
+        await Promise.all([
+          multi.incrby(quantityKey, quantity),
+          multi.incrby(ticketRemainingKey, -quantity),
+        ]);
+        this.logger.log('Finished changing data');
       }
 
-      ticketRemainingData = ticketData.remaining_number;
-      reservedQuantity = quantity;
-
-      if (ticketRemainingData < reservedQuantity) {
-        multi.discard();
-        throw new HttpException('Not enough Tickets', HttpStatus.BAD_REQUEST);
-      }
-
-      ticketRemainingData -= quantity;
-
-      this.logger.log('Accessing to Redis to change data');
-      multi.mset({
-        [ticketKey]: JSON.stringify(ticketData),
-        [ticketRemainingKey]: ticketRemainingData,
-        [quantityKey]: reservedQuantity,
-      });
-      this.logger.log('Finished changing data');
-    } else {
-      if (ticketRemainingData < reservedQuantity) {
-        multi.discard();
-        throw new HttpException('Not enough Tickets', HttpStatus.BAD_REQUEST);
-      }
-      this.logger.log('Accessing to Redis to change data');
-      await Promise.all([
-        multi.incrby(quantityKey, quantity),
-        multi.incrby(ticketRemainingKey, -quantity),
-      ]);
-      this.logger.log('Finished changing data');
-    }
-
-    const results = await multi.exec();
-    const transactionFailed = results.some((result) => result instanceof Error);
-
-    if (transactionFailed) {
-      throw new HttpException(
-        'Transaction failed',
-        HttpStatus.INTERNAL_SERVER_ERROR,
+      const results = await multi.exec();
+      const transactionFailed = results.some(
+        (result) => result instanceof Error,
       );
-    } else {
-      console.log('success');
-    }
 
-    return ticketData;
+      if (transactionFailed) {
+        throw new HttpException(
+          'Transaction failed',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      } else {
+        console.log('success');
+      }
+
+      return ticketData;
+    } catch (error) {
+      this.logger.error('An error occurred while reserving ticket:', error);
+      throw error;
+    }
   }
 
   async issueCoupon(id: string, couponId: number) {
@@ -215,96 +222,110 @@ export class UserService {
   }
 
   async purchase(id: string, ticketId: number, couponId: number) {
-    this.logger.log('Accessing to DB to find user');
-    const user: User = await this.userRepository.findOne({
-      where: { id },
-      relations: ['discountRate'],
-    });
-    this.logger.log('Found user from DB');
+    try {
+      const multi = this.redisService.multi();
 
-    if (!user) {
-      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-    }
+      this.logger.log('Accessing to DB to find user');
+      const user: User = await this.userRepository.findOne({
+        where: { id },
+        relations: ['discountRate'],
+      });
+      this.logger.log('Found user from DB');
 
-    const ticketKey: string = `user:${id}-ticket:${ticketId}`;
-    const quantityKey: string = `${ticketKey}:reservedQuantity`;
-    const couponKey: string = `user:${id}:coupon:${couponId}`;
-    const couponCountKey: string = `${couponKey}:count`;
-    const transactionKey = 'transaction';
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+      }
 
-    this.logger.log('Accessing to Redis to get data');
-    let [ticketData, coupon, couponCount] = await this.redisService
-      .mget(ticketKey, couponKey, couponCountKey)
-      .then((results) =>
-        results.map((item) => (item ? JSON.parse(item) : null)),
+      const ticketKey: string = `user:${id}-ticket:${ticketId}`;
+      const quantityKey: string = `${ticketKey}:reservedQuantity`;
+      const couponKey: string = `user:${id}:coupon:${couponId}`;
+      const couponCountKey: string = `${couponKey}:count`;
+      const transactionKey = 'transaction';
+
+      this.logger.log('Accessing to Redis to get data');
+      let [ticketData, coupon, couponCount] = await this.redisService
+        .mget(ticketKey, couponKey, couponCountKey)
+        .then((results) =>
+          results.map((item) => (item ? JSON.parse(item) : null)),
+        );
+      this.logger.log('Found data from Redis');
+
+      if (!ticketData) {
+        multi.discard();
+        throw new HttpException('Ticket not found', HttpStatus.NOT_FOUND);
+      }
+
+      let appliedPrice: number = ticketData.price;
+
+      if (coupon) {
+        if (coupon.isPercentage) {
+          appliedPrice = Math.ceil(
+            appliedPrice - (appliedPrice * coupon.amount) / 100,
+          );
+        } else {
+          appliedPrice = Math.max(appliedPrice - coupon.amount, 0);
+        }
+
+        this.logger.log('Accessing to Redis to change coupon data');
+        multi.incrby(couponCountKey, -1);
+        this.logger.log('Finished changing coupon data');
+
+        if (couponCount === 0) {
+          this.logger.log('Accessing to Redis to delete coupon data');
+          await Promise.all([multi.del(couponKey), multi.del(couponCountKey)]);
+          this.logger.log('Finished deleting coupon data');
+        }
+      }
+      const discountRatio: number = user.discountRate.discountRatio;
+
+      appliedPrice -= appliedPrice * discountRatio;
+      appliedPrice = Math.ceil(appliedPrice);
+
+      // let reservedQuantity = this.reservedQuantities.get(key);
+      // this.reservedQuantities.delete(key);
+
+      // const newRemaining = ticket.remaining_number - reservedQuantity;
+
+      // await this.ticketRepository.update(ticketId, {
+      //   remaining_number: newRemaining,
+      // });
+
+      const transactionData: Transaction = {
+        id: null,
+        ticketId: ticketId,
+        couponId: coupon ? couponId : null,
+        totalPrice: appliedPrice,
+        quantity: 1,
+        createdAt: new Date(),
+        user: user,
+        startTime: Date.now(),
+      };
+
+      this.logger.log('Accessing to Redis to store transactional data');
+      multi.rpush(transactionKey, JSON.stringify(transactionData));
+      this.logger.log('Finished storing transactional data');
+
+      const results = await multi.exec();
+      const transactionFailed = results.some(
+        (result) => result instanceof Error,
       );
-    this.logger.log('Found data from Redis');
 
-    if (!ticketData) {
-      throw new HttpException('Ticket not found', HttpStatus.NOT_FOUND);
-    }
-
-    let appliedPrice: number = ticketData.price;
-
-    console.log({ coupon, couponCount });
-
-    if (coupon) {
-      if (coupon.isPercentage) {
-        appliedPrice = Math.ceil(
-          appliedPrice - (appliedPrice * coupon.amount) / 100,
+      if (transactionFailed) {
+        throw new HttpException(
+          'Transaction failed',
+          HttpStatus.INTERNAL_SERVER_ERROR,
         );
       } else {
-        appliedPrice = Math.max(appliedPrice - coupon.amount, 0);
+        console.log('success');
       }
 
-      this.logger.log('Accessing to Redis to change coupon data');
-      await this.redisService.incrby(couponCountKey, -1);
-      this.logger.log('Finished changing coupon data');
+      this.rabbitMqService.sendToRabbitMQ(user.id, ticketId, 1, appliedPrice);
 
-      if (couponCount === 0) {
-        this.logger.log('Accessing to Redis to delete coupon data');
-        await Promise.all([
-          await this.redisService.del(couponKey),
-          await this.redisService.del(couponCountKey),
-        ]);
-        this.logger.log('Finished deleting coupon data');
-      }
+      return transactionData;
+    } catch (error) {
+      this.logger.error('An error occurred while processing purchase:', error);
+      throw error;
     }
-    const discountRatio: number = user.discountRate.discountRatio;
-
-    appliedPrice -= appliedPrice * discountRatio;
-    appliedPrice = Math.ceil(appliedPrice);
-
-    // let reservedQuantity = this.reservedQuantities.get(key);
-    // this.reservedQuantities.delete(key);
-
-    // const newRemaining = ticket.remaining_number - reservedQuantity;
-
-    // await this.ticketRepository.update(ticketId, {
-    //   remaining_number: newRemaining,
-    // });
-
-    const transactionData: Transaction = {
-      id: null,
-      ticketId: ticketId,
-      couponId: coupon ? couponId : null,
-      totalPrice: appliedPrice,
-      quantity: 1,
-      createdAt: new Date(),
-      user: user,
-      startTime: Date.now(),
-    };
-
-    this.logger.log('Accessing to Redis to store transactional data');
-    await this.redisService.rpush(
-      transactionKey,
-      JSON.stringify(transactionData),
-    );
-    this.logger.log('Finished storing transactional data');
-
-    this.rabbitMqService.sendToRabbitMQ(user.id, ticketId, 1, appliedPrice);
-    // await this.transactionRepository.save(transactionData);
-    return transactionData;
   }
 
   async getPurchaseHistory(id: string, page: number, size: number) {
