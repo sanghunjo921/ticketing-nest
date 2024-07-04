42cea3d7f71794de65d1e9e34401440f1974ac35
sanghunjo921 <salr921@gmail.com>
2024-07-04 21:31:51 +0900

    basic CRUD for comments

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,141 @@
-import { Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TicketService } from 'src/ticket/ticket.service';
+
+import { UserService } from 'src/user/user.service';
+import { Repository } from 'typeorm';
+import { CreateCommentReqDto, UpdateCommentDto } from './dto/req.dto';
+import { CreateCommentResDto } from './dto/res.dto';
+import { Comment } from './entities/comment.entity';
 
 @Injectable()
-export class CommentService {}
+export class CommentService {
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentRepository: Repository<Comment>,
+    private readonly userService: UserService,
+    @Inject(forwardRef(() => TicketService))
+    private readonly ticketService: TicketService,
+  ) {}
+
+  async createComment({
+    content,
+    level,
+    parent,
+    userId,
+    ticketId,
+  }: CreateCommentReqDto): Promise<CreateCommentResDto> {
+    const user = await this.userService.findOne(userId);
+    const post = await this.ticketService.findOne(ticketId);
+
+    const newComment = this.commentRepository.create({
+      content,
+      level,
+      parent,
+    });
+
+    await this.commentRepository.save(newComment);
+
+    return newComment;
+  }
+
+  //   async getAllCommentsByPost(ticketId: number): Promise<Comment[]> {
+  //     const targetTicket = await this.ticketService.findOne(ticketId);
+
+  //     const comments = targetTicket.comments
+
+  //     comments.sort((commentA, commentB) => {
+  //       return (
+  //         commentA.parent - commentB.parent ||
+  //         commentA.createdAt.getTime() - commentB.createdAt.getTime()
+  //       );
+  //     });
+
+  //     return comments;
+  //   }
+
+  //   async getCommentByPost(id: number, ticketId: number): Promise<Comment> {
+  //     try {
+  //       const targetPost = await this.ticketService.findOne(ticketId);
+
+  //       if (!targetPost) {
+  //         throw new Error('Post not found or already deleted');
+  //       }
+
+  //       const targetComment = targetPost.comments.find(
+  //         (comment) => comment.id === id && comment.isDeleted === 'N',
+  //       );
+
+  //       if (!targetComment) {
+  //         throw new Error('Comment not found or already deleted');
+  //       }
+
+  //       return targetComment;
+  //     } catch (error) {
+  //       throw error;
+  //     }
+  //   }
+
+  async updateComment(
+    id: number,
+    updateDate: UpdateCommentDto,
+  ): Promise<Comment> {
+    try {
+      const targetComment = await this.commentRepository.findOne({
+        where: { id },
+      });
+
+      const { affected } = await this.commentRepository.update(id, updateDate);
+
+      if (affected === 0) {
+        throw new HttpException('Comment not found', HttpStatus.BAD_REQUEST);
+      }
+
+      return { ...targetComment, ...updateDate };
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  //   async deleteCommentsByPostId(postid: number): Promise<any> {
+  //     try {
+  //       const targetPost = await this.ticketService.findPostById(postid);
+
+  //       console.log({ targetPost });
+
+  //       return this.commentRepository.update(
+  //         {
+  //           post: targetPost,
+  //         },
+  //         {
+  //           isDeleted: 'Y',
+  //         },
+  //       );
+  //     } catch (error) {
+  //       throw error;
+  //     }
+  //   }
+
+  async deleteComment(id: number): Promise<Comment> {
+    try {
+      const targetComment = await this.commentRepository.findOne({
+        where: { id },
+      });
+      if (!targetComment) {
+        throw new Error('Comment not found or already deleted');
+      }
+      targetComment.isDeleted = 'Y';
+      await this.commentRepository.save(targetComment);
+
+      return targetComment;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
