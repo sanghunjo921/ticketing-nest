9454dbbd5e1e9fd7c10bae8b9bc3a1bb908448bc
sanghunjo921 <salr921@gmail.com>
2024-07-05 21:36:29 +0900

    implemented CRUD comments based on ticket

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CommentController } from './comment.controller';
 import { CommentService } from './comment.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Comment } from './entities/comment.entity';
+import { User } from 'src/user/entity/user.entity';
+import { Ticket } from 'src/ticket/entity/ticket.entity';
+import { UserModule } from 'src/user/user.module';
+import { TicketModule } from 'src/ticket/ticket.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Comment]), UserModule, TicketModule],
   controllers: [CommentController],
-  providers: [CommentService]
+  providers: [CommentService],
+  exports: [CommentService],
 })
 export class CommentModule {}
